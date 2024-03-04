e5450d07b3805cc0f266566c07b649813a99be38
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-04 23:13:35 +0100

    finished

--- pages/blog/[id]/index.vue
+++ pages/blog/[id]/index.vue
@@ -1,8 +1,37 @@
-<script setup></script>
+<script setup>
+const employees = [
+	{
+		id: 1,
+		firstName: "Frank",
+	},
+	{
+		id: 2,
+		firstName: "Anne",
+	},
+	{
+		id: 3,
+		firstName: "Roger",
+	},
+];
+
+const route = useRoute();
+
+const employee = employees.find((m) => m.id === Number(route.params.id));
+</script>
 
 <template>
 	<h1>Blog Entry</h1>
+	<nav>
+		<ul class="flex gap-1">
+			<li v-for="emp in employees" :key="emp.id">
+				<NuxtLink :to="`${emp.id}`" class="underline"
+					>{{ emp.firstName }}</NuxtLink
+				>
+			</li>
+		</ul>
+	</nav>
 	<p>showing id#{{ $route.params.id }}</p>
+	<p>First Name: {{ employee.firstName }}</p>
 </template>
 
 <style scoped></style>
---
+++ pages/category/[id].vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const employees = [
+	{
+		id: 1,
+		firstName: "Frank",
+	},
+	{
+		id: 2,
+		firstName: "Anne",
+	},
+	{
+		id: 3,
+		firstName: "Roger",
+	},
+];
+
+const route = useRoute();
+
+const employee = employees.find((m) => m.id === Number(route.params.id));
+</script>
+
+<template>
+  <div>
+    {{ employee.firstName }}
+  </div>
+</template>
+
+<style scoped></style>
