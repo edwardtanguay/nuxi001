c0b48e50123200c6926c28c1917a3ca3a3186fc4
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-04 21:01:35 +0100

    id works

---
+++ pages/blog/[id]/index.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+	<h1>Blog Entry</h1>
+	<p>showing id#{{ $route.params.id }}</p>
+</template>
+
+<style scoped></style>
---
+++ pages/info.vue
@@ -0,0 +1,8 @@
+<script lang="ts"
+        setup></script>
+
+<template>
+  <h1 class="text-3xl">Page: Info</h1>
+</template>
+
+<style scoped></style>
