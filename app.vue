47f7a8d91182a7485ec4a1cc684c785de5ff1166
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-04 19:44:16 +0100

    tailwind in

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>Info Site</h1>
-    <p>Welcome to this site.</p>
-  </div>
+  <body class="bg-gray-500 p-6">
+    <div>
+      <h1 class="text-3xl mb-3">Info Site</h1>
+      <p>Welcome to this site.</p>
+    </div>
+  </body>
 </template>
