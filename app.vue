0b706175186496acaa736db2ddc42676b2ffda83
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-04 20:49:28 +0100

    site works

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
+<script setup></script>
+
 <template>
-	<div>
-		<h1 class="text-3xl mb-3">Info Site</h1>
-		<p>Welcome to this site.</p>
-	</div>
+  <NuxtPage/>
 </template>
+
+<style scoped>
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+	<h1 class="text-3xl mb-3">Info Site</h1>
+	<p>Welcome to this site.</p>
+</template>
+
+<style scoped></style>
